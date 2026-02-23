bad02139f36a3539cfa6e4beced68df632ce33e0
YB <Vaibhav@Manojs-MacBook-Air.local>
2026-02-24 00:50:32 +0530

    Revert GH Pages config and prepare for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/sidereal-hawking',
-  images: {
-    unoptimized: true,
-  }
+  /* config options here */
 };
 
 export default nextConfig;
