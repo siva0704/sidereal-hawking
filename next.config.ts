d3806a826c63fab14092a7cfb29b08630266916b
YB <Vaibhav@Manojs-MacBook-Air.local>
2026-02-24 00:48:19 +0530

    Configure GitHub Pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/sidereal-hawking',
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
